b7467bd4d480dede912fd94c69d927cf253fa517
kiranchinni7 <kirank15c@gmail.com>
2022-08-25 17:13:29 +0530

    first commit

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,9 +1,13 @@
 #!bin/bash
 
+#first line should be SHEBANG and always be only one SHEBANG notation apart from that # will be refferd as comment
 echo "hello world"
-
 
 #printing the multiple lines
 
 echo "hai"
 echo "hello"
+
+## \n : new line  ; ## \t : tab space
+
+echo-e "hai\nhello"
