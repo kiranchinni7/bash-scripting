efe3106216eed67f4903e123120fccfb2c913da5
kiranchinni7 <kirank15c@gmail.com>
2022-08-26 11:26:43 +0530

    this is my first commit

--- 01-sample.sh
+++ 01-sample.sh
@@ -11,3 +11,26 @@ echo "hello"
 ## \n : new line  ; ## \t : tab space
 
 echo-e "hai\nhello"
+
+## printing the words in different lines with tab space  for second new line
+echo-e "hai\n\thello"
+
+# text coloring
+
+# echo -e "\e[COL-COEDm  message \e[0m"
+
+echo -e "\e[33m i am printing colors \e[0m"
+
+#backgroun color
+# echo -e "\e[background; COL-COEDm  message \e[0m"
+echo -e "\e[35;33m i am printing colors \e[0m"
+
+#variable is something whioch holds the value dynamically
+a=10
+#10 is a string
+b=abc
+#abc also a string
+
+#when u use double quotes ?? when your input having any special characters, then you need to enclose in QUOTES.
+echo $a # $a is printing the value of a
+echo $b
